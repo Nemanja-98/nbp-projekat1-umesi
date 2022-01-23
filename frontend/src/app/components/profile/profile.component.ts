4de1b1a63d1adced61585f554cd0d9de4c1a8dd7
Nemanja Momcilovic <hornhuff@hotmail.com>
2022-01-23 01:29:23 +0100

    added profile create new post, connected endpoints for user service routes

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -13,14 +13,19 @@ export class ProfileComponent implements OnInit {
   //to do update later
   public username = "neki username"
   public displayedList :Array<string> = [];
-  
+  public showCreateNewRecipe : boolean = false;
+
+  public recipeTitle :string = '';
+  public recipeDesc :string = '';
+  public recipeIngredients :string = '';
   
   constructor(private userService : UserService) {
     this.displayedList = this.userService.getUserPosts(this.username);
     
     this.userService.user.subscribe( (user :User) => {
+      console.log("nije vise neki username");
       if(user)
-        this.username = user.username;
+        this.username = user.username;//localStorage.getItem('username');
     })
   }
 
@@ -28,14 +33,25 @@ export class ProfileComponent implements OnInit {
   }
 
   showFavouritePosts(){
+    this.showCreateNewRecipe = false;
     this.displayedList = this.userService.getUserFavouritePosts(this.username);
   }
 
-  showUserComments(){
-    this.displayedList = this.userService.getUserComments(this.username);
+  showFollowedUsers(){
+    this.showCreateNewRecipe = false;
+    this.displayedList = this.userService.getFollowedUsers(this.username);
   }
   
   showUserPosts(){
+    this.showCreateNewRecipe = false;
     this.displayedList = this.userService.getUserPosts(this.username);
+  }
+
+  showCreateNewRecipeForm(){
+    this.showCreateNewRecipe = true;
+  }
+
+  createNewPost(){
+    //http request preko post servis-a
   }
 }
--- frontend/src/app/services/user/user.service.ts
+++ frontend/src/app/services/user/user.service.ts
@@ -1,9 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { User } from 'src/app/models/user';
 
+const baseUrl = 'https://localhost:7084/api' ;
+const service = {
+  'Auth': "Auth",
+  'User': "User",
+};
 
 interface logInSessionToken{
   token : string;
@@ -34,7 +39,7 @@ export class UserService {
     console.log("logging in");
     
     const post =  this.http
-    .post('https://localhost:7084/api/auth/login', {
+    .post('https://localhost:7084/api/Auth/login', {
        "Username":username,
       "Password": password,
     },{headers: {
@@ -48,12 +53,25 @@ export class UserService {
       const token = data.token;
       console.log("tokenche",token);
       localStorage.setItem('token', token );
-      localStorage.setItem('username', username)
+      localStorage.setItem('username', username);
+      this.setUserObservable(token);
       this.loggedIn = true;
       this.router.navigate(['']);
     })
     return  post;
       
+  }
+  setUserObservable(token){
+    const username = localStorage.getItem('username');
+    const response = this.http
+    .get<User>('https://localhost:7084/api/User/GetUser/'+username,{headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization' : token
+    }});
+    console.log("resposne is user", response);
+    this.user = response;
   }
 
    logout() {
@@ -67,7 +85,11 @@ export class UserService {
 
   register(user: User) {
     console.log("registering",user.username,user.password,user);
-    return this.http.post("api/user/adduser", user);
+    return this.http.post("https://localhost:7084/api/User/AddUser", user, {headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }});
   }
 
   getUserPosts(username : string){
@@ -76,16 +98,20 @@ export class UserService {
     return ["post 1", "post 2", "post 3", "post 4","post 5","post 6","post 7","post 8","post 9","post 10",];
   }
 
-  getUserComments(username : string){
+  getFollowedUsers(username : string){
     //http request here
 
-    return ["comment 1", "comment 2", "comment 3"];
+    return ["getFollowedUsers 1", "getFollowedUsers 2", "getFollowedUsers 3"];
   }
   
   getUserFavouritePosts(username : string){
     //http request here
 
     return ["favourite post 1", "favourite post 2", "favourite post 3"];
+  }
+
+  subscribeToUser(username){
+    //http request here
   }
  
 }
