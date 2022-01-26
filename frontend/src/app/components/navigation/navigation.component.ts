a78a1d14b9195d15788d8c322809275f2845090b
Nemanja Momcilovic <hornhuff@hotmail.com>
2022-01-26 21:31:17 +0100

    changed navigation random post to navigate to a random post between 1-5

--- frontend/src/app/components/navigation/navigation.component.ts
+++ frontend/src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
@@ -8,12 +9,20 @@ import { UserService } from 'src/app/services/user/user.service';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor(public userService : UserService) { }
+  constructor(public userService : UserService, public router :Router) { }
 
   ngOnInit(): void {
   }
 
   logout(): void{
     this.userService.logout()
+  }
+
+  randomRecipeClicked(){
+    const id : number = Math.round(Math.random()*10) % 5;
+    this.router.navigate([`/profile`]);
+    setTimeout(() => {
+      this.router.navigateByUrl(`post/${id}`);
+    }, 0);
   }
 }
